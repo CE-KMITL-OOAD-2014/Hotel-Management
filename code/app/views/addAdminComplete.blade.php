d8059686b594dbda7c899d487748ad4879383611
Wikrom Phuchan <mac.wikrom@gmail.com>
2014-10-28 21:26:33 +0700

    renew code

--- code/app/views/addAdminComplete.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout')
-
-@section('body')
-<div class="alert alert-success" role="alert">Create admin complete! Use the button below to return to login.</div>
-<a href="/"><button type="button" class="btn btn-default" style="background-color:#31152B; color:white;">Return to login</button></a>
-@stop
