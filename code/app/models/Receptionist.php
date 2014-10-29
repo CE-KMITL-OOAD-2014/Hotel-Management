693c72615865352c1afe05c0aadef686a2af64f6
Wikrom Phuchan <mac.wikrom@gmail.com>
2014-10-30 01:10:07 +0700

    unit Test (it's too hard to know what I really have to know before code the testing code T___T)

---
+++ code/app/models/Receptionist.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+* 
+*/
+class Receptionist extends Staff
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+//-----------------------------------------
+
+
+//-----------------------------------------
+}
+ ?>
